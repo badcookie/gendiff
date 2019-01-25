0825662101116002fa259d3066eeb846ad733f48
Polina Novokreschenova <noverianhenon@yandex.ru>
2019-01-25 14:53:10 +0300

    update tests & fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,18 +11,17 @@ const getRelativeFilePath = (filename) => {
   return path.join(commonFixturePath, filename);
 };
 
-const check = ({ fileBefore, fileAfter, fileExpected }) => {
+const check = ({ fileBefore, fileAfter }) => {
   const filepath1 = getAbsoluteFilePath(fileBefore);
   const filepath2 = getAbsoluteFilePath(fileAfter);
-  const actual = genDiff(filepath1, filepath2);
-  const expected = fs.readFileSync(getRelativeFilePath(fileExpected), 'utf-8');
-  expect(actual).toEqual(expected);
+  const actual = genDiff(filepath1, filepath2, 'sample');
+  const expected = fs.readFileSync(getRelativeFilePath('expected.txt'), 'utf-8');
+  expect(actual).toBe(expected);
 };
 
 test.each`
-  fileBefore        | fileAfter         | fileExpected       | format
-  ${'config1.json'} | ${'config2.json'} | ${'expected1.txt'} | ${'.json'}
-  ${'config3.json'} | ${'config4.json'} | ${'expected2.txt'} | ${'.json'}
-  ${'config5.yml'}  | ${'config6.yaml'} | ${'expected1.txt'} | ${'.yml/.yaml'}
-  ${'config7.ini'}  | ${'config8.ini'}  | ${'expected2.txt'} | ${'.ini'}
-`('Check absolute paths for $format format', check);
+  fileBefore        | fileAfter         | format
+  ${'config1.json'} | ${'config2.json'} | ${'.json'}
+  ${'config3.yml'}  | ${'config4.yml'}  | ${'.yml/.yaml'}
+  ${'config5.ini'}  | ${'config6.ini'}  | ${'.ini'}
+`('Check recursive rendering for $format format', check);
