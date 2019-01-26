fb905b92f8b0d07a1eac255f05f41b6be06d4b43
Polina Novokreschenova <noverianhenon@yandex.ru>
2019-01-26 22:24:17 +0300

    update tests & fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,17 +11,22 @@ const getRelativeFilePath = (filename) => {
   return path.join(commonFixturePath, filename);
 };
 
-const check = ({ fileBefore, fileAfter }) => {
+const check = ({
+  fileBefore, fileAfter, fileExpected, outputFormat,
+}) => {
   const filepath1 = getAbsoluteFilePath(fileBefore);
   const filepath2 = getAbsoluteFilePath(fileAfter);
-  const actual = genDiff(filepath1, filepath2, 'sample');
-  const expected = fs.readFileSync(getRelativeFilePath('expected.txt'), 'utf-8');
+  const actual = genDiff(filepath1, filepath2, outputFormat);
+  const expected = fs.readFileSync(getRelativeFilePath(fileExpected), 'utf-8');
   expect(actual).toBe(expected);
 };
 
 test.each`
-  fileBefore        | fileAfter         | format
-  ${'config1.json'} | ${'config2.json'} | ${'.json'}
-  ${'config3.yml'}  | ${'config4.yml'}  | ${'.yml/.yaml'}
-  ${'config5.ini'}  | ${'config6.ini'}  | ${'.ini'}
-`('Check recursive rendering for $format format', check);
+  fileBefore        | fileAfter         | fileExpected    | inputFormat     | outputFormat
+  ${'config1.json'} | ${'config2.json'} | ${'plain.txt'}  | ${'.json'}      | ${'plain'}
+  ${'config1.json'} | ${'config2.json'} | ${'sample.txt'} | ${'.json'}      | ${'sample'}
+  ${'config3.yml'}  | ${'config4.yml'}  | ${'plain.txt'}  | ${'.yml/.yaml'} | ${'plain'}
+  ${'config3.yml'}  | ${'config4.yml'}  | ${'sample.txt'} | ${'.yml/.yaml'} | ${'sample'}
+  ${'config5.ini'}  | ${'config6.ini'}  | ${'plain.txt'}  | ${'.ini'}       | ${'plain'}
+  ${'config5.ini'}  | ${'config6.ini'}  | ${'sample.txt'} | ${'.ini'}       | ${'sample'}
+`('Check rendering for $inputFormat format to compare with $outputFormat format', check);
